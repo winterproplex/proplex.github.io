17a61bc65725da2f966c9edc03ff3bc37942f82d
winterproplex <dat1chick4life@gmail.com>
2016-05-18 18:39:28 -0400

    major

---
+++ client/config.js
@@ -0,0 +1,4 @@
+// upload.config.server = '' // Empty if the webapp is in the same place as the server
+// upload.config.server = '' // If the webapp is separated from the server - remember the trailing slash
+// upload.config.api_key = '' // Should be the same as the server, used for uploading
+// upload.config.footer = ' |- <a href="mailto:proplexgaming@gmail.com" id="contact" target="_blank">Contact</a> -|'
